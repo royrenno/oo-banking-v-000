135cd720dcdf3598949fb9163fb031308701bce9
roy renno <royrenno@yahoo.com>
2019-05-04 01:01:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,17 @@
 class BankAccount
   attr_accessor :balace, :status
   attr_reader :name
+  
   def initialize(account_name)
     @name = account_name
     @balance = 1000
     @status = "open"
   end
 
+  def deposit(money)
+@balance += money
+end
 
+def display_balance
+  "Your balance is $#{@balance}."
 end
