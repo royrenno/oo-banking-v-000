29fd751fc8109c607b1ce485cd9e2c11f3c65f48
roy renno <royrenno@yahoo.com>
2019-05-04 01:16:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
+  attr_accessor :sender, :receiver,:amount, :status
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
   # your code here
 end
